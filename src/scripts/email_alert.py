0bc04164abac4d9b9f79f4fd1c999fd82374f92d
VHS <listas.vhs@gmail.com>
2024-12-14 13:54:35 +0100

    Refactor email_alert scripts to resolve ShellCheck issues.

--- src/scripts/email_alert.py
+++ src/scripts/email_alert.py
@@ -1,144 +1,159 @@
 #!/usr/bin/env python
-#################################################################################
-#                                                                               #
-# iscsi-ha - High Availability framework for iSCSI cluster used in conjunction  #
-# with XAPI based Xen Virtualization Environment (Xen Cloud Platform/XenServer) #
-# Copyright 2024 Salvatore Costantino                                           #
-# ha@ixi0.com                                                                   #
-#                                                                               #
-#                                                                               #
-#    iscsi-ha is free software: you can redistribute it and/or modify           #
-#    it under the terms of the GNU General Public License as published by       #
-#    the Free Software Foundation, either version 3 of the License, or          #
-#    (at your option) any later version.                                        #
-#                                                                               #
-#    iscsi-ha is distributed in the hope that it will be useful,                #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of             #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
-#    GNU General Public License for more details.                               #
-#                                                                               #
-#    You should have received a copy of the GNU General Public License          #
-#    along with iscsi-ha.  If not, see <http://www.gnu.org/licenses/>.          #
-#                                                                               #
-#################################################################################
 
-import sys, socket, smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+import sys
+import socket
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
-##########################
-# Suppress stderr in case
-# network/DNS  is down
-##########################
-#null = open('/dev/null', 'w')
-#sys.stderr = null
-
-###########################
-# if network or DNS is down
-# dont wait - exit 1
-###########################
+# Set a timeout for socket operations to avoid hanging if the network or DNS is down
 socket.setdefaulttimeout(2)
 
-if len(sys.argv) != 11:
-	print ("HA-Lizard email alert sender. Version 1.0 www.halizard.com")
-	print ("Pass in all command line arguments as follows")
-	print ("<command> from_email to_email subject timestamp process_name message_body smtp_server smtp_port smtp_user smtp_pass")
-	sys.exit(1)
 
-#############################
-#Declare System Hostname
-#############################
-hostname = socket.gethostname()
+def print_usage():
+    """Print usage instructions for the script."""
+    print("HA-Lizard email alert sender. Version 1.0 www.halizard.com")
+    print("Pass in all command line arguments as follows")
+    print(f"{sys.argv[0]} from_email to_email subject timestamp process_name message_body smtp_server smtp_port smtp_user smtp_pass")
 
-#############################
-# Declare passed in args
-#############################
-from_email = str(sys.argv[1])
-to_email = str(sys.argv[2])
-subject = str(sys.argv[3])
-timestamp = str(sys.argv[4])
-process_name = str(sys.argv[5])
-message_body = str(sys.argv[6])
-smtp_server = str(sys.argv[7])
-smtp_port =  str(sys.argv[8])
-smtp_user =  str(sys.argv[9])
-smtp_pass =  str(sys.argv[10])
 
-#############################
-# Echo to stdout - halizard
-# redirects to log
-#############################
-print "Sending email from: "+from_email
-print "Sending email to: "+to_email
-print "Email Alert Subject: "+subject
-print "Email Alert Timestamp: "+timestamp
-print "Email Alert Process: "+process_name
-print "Email Alert Message Content: "+message_body
-print "Email Alert Message Hostname: "+hostname
+def get_hostname():
+    """Return the system hostname."""
+    return socket.gethostname()
 
-###############################
-# Create the message headers
-###############################
-msg = MIMEMultipart('alternative')
-msg['Subject'] = subject
-msg['From'] = from_email
-msg['To'] = to_email
 
-# Create the body of the message (a plain-text and an HTML version).
-###############################
-# Message body - Text
-###############################
-text = message_body
+def construct_message(from_email, to_email, subject, message_body, process_name, hostname, timestamp):
+    """
+    Construct the email message with plain text and HTML parts.
 
-###############################
-# Message body - HTML
-###############################
-html = """\
-<table height="100" cellspacing="1" cellpadding="1" border="1" width="600" style="">
-    <tbody>
-        <tr>
-            <td width="160"><img height="111" width="150" src="http://www.halizard.com/images/ha_lizard_alert_logo.png" alt="" /></td>
-            <td width="440"><span style="color: rgb(0, 102, 0);"><strong><span style="font-size: larger;"><span style="font-family: Arial;">HA-Lizard Alert Notification<br />
-            <br />
-            Process: %s <br />
-            Host: %s <br />
-            Time: %s </span></span></strong></span></td>
-        </tr>
-        <tr>
-            <td width="600" colspan="2">
-            <p><br />
-            <span style="font-family: Arial;"><span style="font-size: smaller;"> %s <br />
-            <br />
-            </span></span></p>
-            </td>
-        </tr>
-        <tr>
-            <td bgcolor="#cccccc" width="600" colspan="2">
-            <p style="text-align: left;"><strong><span style="font-size: smaller;"><span style="font-family: Arial;">website</span></span></strong><span style="font-size: smaller;"><span style="font-family: Arial;">: www.halizard.com&nbsp;&nbsp;&nbsp;&nbsp; <strong>forum</strong>: http://www.halizard.com/forum</span></span>&nbsp;&nbsp;&nbsp; <strong><span style="font-size: smaller;"><span style="font-family: Arial;">Sponsored by</span></span></strong><span style="font-size: smaller;"><span style="font-family: Arial;"> </span></span><a href="http://www.pulsesupply.com"><span style="font-size: smaller;"><span style="font-family: Arial;">Pulse Supply</span></span></a></p>
-            </td>
-        </tr>
-    </tbody>
-</table>
-<p>&nbsp;</p>
-""" % (process_name, hostname, timestamp, message_body)
+    Args:
+        from_email (str): Sender's email address.
+        to_email (str): Receiver's email address.
+        subject (str): Subject of the email.
+        message_body (str): Body content of the email.
+        process_name (str): Name of the process triggering the alert.
+        hostname (str): System hostname.
+        timestamp (str): Timestamp of the alert.
 
-#############################
-# Construct the message
-#############################
-text_part = MIMEText(text, 'plain')
-html_part = MIMEText(html, 'html')
-msg.attach(text_part)
-msg.attach(html_part)
+    Returns:
+        MIMEMultipart: The email message object.
+    """
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = formataddr(('HA-Lizard', from_email))
+    msg['To'] = to_email
 
-###############################
-# Send the message
-###############################
-if smtp_port == "465":
-	message = smtplib.SMTP_SSL(smtp_server, smtp_port, hostname)
-else:
-	message = smtplib.SMTP(smtp_server, smtp_port, hostname)
-message.set_debuglevel(9)
-if len(sys.argv[9]) > 1:
-	message.login(smtp_user, smtp_pass)
-message.sendmail(from_email, to_email, msg.as_string())
-message.quit()
+    # Message body - Text
+    text = message_body
+
+    # Message body - HTML
+    message_body_html = "<br />".join(message_body.split("\n"))
+    html = """\
+    <table height="100" cellspacing="1" cellpadding="1" border="1" width="600" style="">
+        <tbody>
+            <tr>
+                <td width="160"><img height="111" width="150" src="http://www.halizard.com/images/ha_lizard_alert_logo.png" alt="" /></td>
+                <td width="440"><span style="color: rgb(0, 102, 0);"><strong><span style="font-size: larger;"><span style="font-family: Arial;">HA-Lizard Alert Notification<br />
+                <br />
+                Process: %s <br />
+                Host: %s <br />
+                Time: %s </span></span></strong></span></td>
+            </tr>
+            <tr>
+                <td width="600" colspan="2">
+                <p><br />
+                <span style="font-family: Arial;"><span style="font-size: smaller;"> %s <br />
+                <br />
+                </span></span></p>
+                </td>
+            </tr>
+            <tr>
+                <td bgcolor="#cccccc" width="600" colspan="2">
+                <p style="text-align: left;"><strong><span style="font-size: smaller;"><span style="font-family: Arial;">website</span></span></strong><span style="font-size: smaller;"><span style="font-family: Arial;">: www.halizard.com&nbsp;&nbsp;&nbsp;&nbsp; <strong>forum</strong>: http://www.halizard.com/forum</span></span>&nbsp;&nbsp;&nbsp; <strong><span style="font-size: smaller;"><span style="font-family: Arial;">Sponsored by</span></span></strong><span style="font-size: smaller;"><span style="font-family: Arial;"> </span></span><a href="http://www.pulsesupply.com"><span style="font-size: smaller;"><span style="font-family: Arial;">Pulse Supply</span></span></a></p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <p>&nbsp;</p>
+    """ % (process_name, hostname, timestamp, message_body_html)
+
+    text_part = MIMEText(text, 'plain')
+    html_part = MIMEText(html, 'html')
+    msg.attach(text_part)
+    msg.attach(html_part)
+
+    return msg
+
+
+def send_email(from_email, to_email, msg, smtp_server, smtp_port, smtp_user, smtp_pass):
+    """
+    Send the email using SMTP.
+
+    Args:
+        from_email (str): Sender's email address.
+        to_email (str): Receiver's email address.
+        msg (MIMEMultipart): The email message object.
+        smtp_server (str): SMTP server address.
+        smtp_port (str): SMTP server port.
+        smtp_user (str): SMTP username for authentication.
+        smtp_pass (str): SMTP password for authentication.
+    """
+    try:
+        if smtp_port == "465":
+            server = smtplib.SMTP_SSL(smtp_server, smtp_port)
+        else:
+            server = smtplib.SMTP(smtp_server, smtp_port)
+
+        server.set_debuglevel(0)  # Set to 0 in production
+
+        if smtp_user and smtp_pass:
+            server.login(smtp_user, smtp_pass)
+
+        server.sendmail(from_email, to_email, msg.as_string())
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+    finally:
+        server.quit()
+
+
+def main():
+    """Main function to execute the script logic."""
+    if len(sys.argv) != 11:
+        print_usage()
+        sys.exit(1)
+
+    # Collect arguments from the command line
+    from_email = sys.argv[1]
+    to_email = sys.argv[2]
+    subject = sys.argv[3]
+    timestamp = sys.argv[4]
+    process_name = sys.argv[5]
+    message_body = sys.argv[6]
+    smtp_server = sys.argv[7]
+    smtp_port = sys.argv[8]
+    smtp_user = sys.argv[9]
+    smtp_pass = sys.argv[10]
+
+    # Echo to stdout - halizard redirects to log
+    print(f"Sending email from: {from_email}")
+    print(f"Sending email to: {to_email}")
+    print(f"Email Alert Subject: {subject}")
+    print(f"Email Alert Timestamp: {timestamp}")
+    print(f"Email Alert Process: {process_name}")
+    print(f"Email Alert Message Content: {message_body}")
+    print(f"Email Alert Message Hostname: {get_hostname()}")
+
+    # Construct and send the email
+    try:
+        msg = construct_message(from_email, to_email, subject,
+                                message_body, process_name, get_hostname(), timestamp)
+        send_email(from_email, to_email, msg, smtp_server,
+                   smtp_port, smtp_user, smtp_pass)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
